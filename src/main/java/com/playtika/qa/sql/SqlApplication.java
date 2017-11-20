0d6cd1e1e8ed6494685fee8ff56ca8097a4f1596
vbazylevych <608levlerka>
2017-11-20 20:38:14 +0200

    Initial commit

---
+++ src/main/java/com/playtika/qa/sql/SqlApplication.java
@@ -0,0 +1,12 @@
+package com.playtika.qa.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlApplication.class, args);
+	}
+}
